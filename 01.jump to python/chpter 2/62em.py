257b9a9250ab8661192f1009eeb280afd4f3e256
wql7654 <47405691+wql7654@users.noreply.github.com>
2019-02-07 11:09:40 +0900

    Vim 작업 디렉토리 반영

---
+++ 01.jump to python/chpter 2/62em.py
@@ -0,0 +1,12 @@
+
+a=['life','is','too','hard']
+re=" ".join(a)
+print(re)
+re=re.split()
+print(re)
+re=','.join(a)
+print(re)
+re=re.split(',')
+print(re)
+re.sort()
+print(re)
---
+++ 01.jump to python/chpter 3/111.py
@@ -0,0 +1,6 @@
+money=0
+if money: #조건문이 변수인 경우
+    print("taxita")
+else:
+    print("walkhaera")
+
