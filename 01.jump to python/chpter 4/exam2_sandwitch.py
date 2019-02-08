9f14529175f38e1bf47d6b33c6c0fd42c870b8de
wql7654 <47405691+wql7654@users.noreply.github.com>
2019-02-08 17:12:38 +0900

    02/08 문자열 입출력

---
+++ 01.jump to python/chpter 4/exam2_sandwitch.py
@@ -0,0 +1,41 @@
+
+sandwich_input=[]
+ingredient_list=''
+order=0
+ixc=0
+def input_ingredient() :
+    while True:
+
+        sandwich_input=input("안녕하세요. 원하시는 재료를 입력하세요:")
+        if sandwich_input=='종료':
+            break
+        ingredient_list=str(ingredient_list)+str(sandwich_input)
+        print(ingredient_list)
+
+    return  ingredient_list
+
+def make_sandwiches(make):
+    print("샌드위치를 만들겠습니다")
+    for make_sand in make:
+        print("%s 추가합니다."%make_sand)
+    print("여기 주문하신 샌드위치 만들었습니다. 맛있게 드세요.")
+
+
+
+
+
+
+while True:
+    order=int(input("안녕하세요. 저희 가게에 방문해 주셔서 감사합니다.\n 1.주문 \n 2.종료\n 입력:"))
+    if order==2:
+        break
+    elif order==1:
+        make=input_ingredient()
+        make_sandwiches(make)
+        break
+    else:
+        continue
+
+
+
+
---
+++ 01.jump to python/chpter 4/exam4_visit.py
@@ -0,0 +1,15 @@
+def search_visitor(name):
+    f = open("./방명록.txt", 'r', encoding="UTF-8")
+    ser_name=f.readlines()
+    f.close()
+
+    for name for ser_name:
+
+
+
+
+
+
+name_vister=input("이름을 입력하세요:")
+
+
