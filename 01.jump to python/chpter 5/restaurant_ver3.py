1379d5bd94f0ae92873749609972527cf034a90f
lee <>
2019-02-11 22:28:35 +0900

    2/11 연습문제 수정

--- 01.jump to python/chpter 5/restaurant_ver3.py
+++ 01.jump to python/chpter 5/restaurant_ver3.py
@@ -1,6 +1,4 @@
 class Restaurant:
-
-
     def __init__(self, name):
         self.number_served = 0
         self.sel_name=name.split()
@@ -20,33 +18,38 @@ class Restaurant:
 
 
 class Restaurant_count:
-    def __init__(self,number):
-        self.number_served =number
+    number_served = 0
     def reset_number_served(self, number):
         print("손님 %s명 들어오셨습니다. 자리를 안내해 드리겠습니다."%number)
 
     def increment_number_served(self, number):
         self.number_served += number
-        print("손님 %s명 들어오셨습니다. 자리를 안내해 드리겠습니다." % self.number_served)
 
     def check_customer_number(self):
         print("지금까지 총 %s명 손님께서 오셨습니다." % self.number_served)
+
+    def zero_number_served(self):
+        self.number_served=0
+        print("손님 카운팅을 0으로 초기화 하였습니다")
+
 
 
 # name_food=Restaurant(str(input("레스토랑 이름과 요리 종류를 선택하세요.(공백으로 구분)")))
 name_food=Restaurant("딩호와 짬뽕")
 name_food.describe_restaurant()
 name_food.open_restaurant()
-
+conts=Restaurant_count()
 while True:
-    number_sev = Restaurant_count(input("어서오세요. 몇명이십니까?(초기화:0입력,종료:-1,누적고객 확인:p) : "))
+    number_sev = (input("어서오세요. 몇명이십니까?(초기화:0입력,종료:-1,누적고객 확인:p) : "))
+
     print(number_sev)
     if number_sev == 'p':
-        number_sev.check_customer_number()
+        conts.check_customer_number()
     elif number_sev == '-1':
         quit()
     elif number_sev == '0':
-        print("손님 카운팅을 0으로 초기화 하였습니다" )
+        conts.zero_number_served()
     else:
-        number_sev.reset_number_served(int(number_sev))
+        conts.reset_number_served(int(number_sev))
+        conts.increment_number_served(int(number_sev))
 
--- 01.jump to python/chpter 5/restaurant_ver4.py
+++ 01.jump to python/chpter 5/restaurant_ver4.py
@@ -1,14 +1,4 @@
 
-def search_log():
-    f = open("./고객서빙현황로그.txt", 'r', encoding="UTF-8")
-    ser_num=f.readlines()
-    f.close()
-    return int(ser_num)
-
-def write_log(ser_num):
-    f = open("./고객서빙현황로그.txt", 'w', encoding="UTF-8")
-    f.write(ser_num)
-    f.close()
 
 class Restaurant:
     def __init__(self, name):
@@ -30,38 +20,52 @@ class Restaurant:
 
 
 class Restaurant_count:
-    def __init__(self,number):
-        self.number_served =search_log()
+    number_served = 0
+    todays_customer = 0
+    def search_log(self):
+        f = open("./고객서빙현황로그.txt", 'r', encoding="UTF-8")
+        self.number_served = f.readlines()
+        f.close()
+
+    def write_log(ser_num):
+        f = open("./고객서빙현황로그.txt", 'w', encoding="UTF-8")
+        f.write(ser_num)
+        f.close()
+
     def reset_number_served(self, number):
         print("손님 %s명 들어오셨습니다. 자리를 안내해 드리겠습니다."%number)
 
     def increment_number_served(self, number):
-        self.number_served += number
-        print("손님 %s명 들어오셨습니다. 자리를 안내해 드리겠습니다." % self.number_served)
+        self.todays_customer  += number
 
     def check_customer_number(self):
-        print("지금까지 총 %s명 손님께서 오셨습니다." % self.number_served)
+        print("지금까지 총 %s명 손님께서 오셨습니다." % self.todays_customer )
+
+    def zero_number_served(self):
+        self.todays_customer =0
+        print("손님 카운팅을 0으로 초기화 하였습니다")
     def __del__(self):
-        write_log(search_log()+self.number_served)
+        write_log(self.number_served+self.todays_customer )
+
 
 
 # name_food=Restaurant(str(input("레스토랑 이름과 요리 종류를 선택하세요.(공백으로 구분)")))
 name_food=Restaurant("딩호와 짬뽕")
 name_food.describe_restaurant()
 name_food.open_restaurant()
-
-
-
+conts=Restaurant_count()
 while True:
-    number_sev = Restaurant_count(input("어서오세요. 몇명이십니까?(초기화:0입력,종료:-1,누적고객 확인:p) : "))
-    # print(number_sev)
+    number_sev = (input("어서오세요. 몇명이십니까?(초기화:0입력,종료:-1,누적고객 확인:p) : "))
+
+    print(number_sev)
     if number_sev == 'p':
-        number_sev.check_customer_number()
+        conts.check_customer_number()
     elif number_sev == '-1':
         quit()
     elif number_sev == '0':
-        print("손님 카운팅을 0으로 초기화 하였습니다" )
-
+        conts.zero_number_served()
     else:
-        number_sev.increment_number_served(int(number_sev))
+        conts.reset_number_served(int(number_sev))
+        conts.increment_number_served(int(number_sev))
+
 
