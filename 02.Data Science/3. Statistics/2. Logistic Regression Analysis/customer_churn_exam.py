0bcdea356b3c98ce7f874442a243917c93a56caa
wql7654 <47405691+wql7654@users.noreply.github.com>
2019-03-15 16:49:23 +0900

    liner

--- 02.Data Science/3. Statistics/2. Logistic Regression Analysis/customer_churn_exam.py
+++ 02.Data Science/3. Statistics/2. Logistic Regression Analysis/customer_churn_exam.py
@@ -13,31 +13,26 @@ churn.columns = [heading.lower() for heading in \
 churn['churn01'] = np.where(churn['churn'] == 'True.', 1., 0.)
 
 
-colums_list = ['Area_Code','VMail_Message',
+colums_list = ['Area_Code','VMail_Message','intl_plan','vmail_plan','account_length','custserv_calls',
                'Day_Mins','Day_Calls','Day_Charge','Eve_Mins','Eve_Calls','Eve_Charge','Night_Mins',
                'Night_Calls','Night_Charge','Intl_Mins','Intl_Calls','Intl_Charge']
 
 dependent_variable = churn['churn01']
 
-intl_dummies = pd.get_dummies(churn['intl_plan'], prefix='intl_plan')
-vmail_dummies = pd.get_dummies(churn['vmail_plan'], prefix='vmail_plan')
+churn['intl_plan'] = np.where(churn['intl_plan'] == 'yes', 1, 0)
+churn['vmail_plan'] = np.where(churn['vmail_plan'] == 'yes', 1, 0)
+
 count_all=0
 all_time=[]
-for num in range(1,18):
+all_time2=[]
+all_time3=[]
+for num in range(1,5):
     combi_list = list(combinations(colums_list, num))
     for tuple_num in combi_list:
         count=0
         alllist=[]
         for lens_num in tuple_num:
             lens_num=lens_num.lower()
-            # if count==0 and lens_num=='intl_plan':
-            #     churn['total_charges']=intl_dummies
-            # elif count!=0 and lens_num=='intl_plan':
-            #     churn['total_charges']=churn['total_charges']+intl_dummies
-            # elif count==0 and lens_num=='vmail_plan':
-            #     churn['total_charges']=vmail_dummies
-            # elif count!=0 and lens_num=='vmail_plan':
-            #     churn['total_charges']=churn['total_charges']+vmail_dummies
             if count==0:
                 churn['total_charges']=churn[lens_num]
             else:
@@ -45,7 +40,7 @@ for num in range(1,18):
             alllist.append(lens_num)
             count += 1
 
-        independent_variables = churn[['account_length', 'custserv_calls', 'total_charges']]
+        independent_variables = churn[['total_charges']]
         independent_constant = sm.add_constant(independent_variables, prepend=True)
         logit_model = sm.Logit(dependent_variable, independent_constant).fit()
 
@@ -67,16 +62,27 @@ for num in range(1,18):
             if i == churn['churn'][all_on]:
                 false_on += 1
             all_on += 1
+
         print(alllist)
         print("정답률 %.2f%%" % ((false_on / all_on) * 100))
-        all_time.append((false_on / all_on) * 100)
+        if count_all<60000:
+            all_time.append((false_on / all_on) * 100)
+        elif count_all<120000:
+            all_time2.append((false_on / all_on) * 100)
+        elif count_all<180000:
+            all_time3.append((false_on / all_on) * 100)
+
         count_all+=1
 
 
 match_dic = sorted(all_time,reverse=True)
-match_dic2 = sorted(all_time,reverse=False)
+match_dic2 = sorted(all_time2,reverse=True)
+match_dic3 = sorted(all_time3,reverse=True)
+
+
 
 print(match_dic)
 print('총 조합 갯수: %d'%count_all)
 print("MAX 조합:",match_dic[0])
 print("MAX2 조합:",match_dic2[0])
+print("MAX3 조합:",match_dic3[0])
